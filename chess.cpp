27c4ce2981a8f37c286cefc19fc8d239a15ebc62
thetateman <tdsmith2018@gmail.com>
2020-11-26 21:11:15 -0600

    generateMoveSet working for N and R

--- chess.cpp
+++ chess.cpp
@@ -49,7 +49,7 @@ int initBoard(vector<vector<square>> *inputBoard) // initialize all the pieces i
 	// initialize back row of black pieces
 
 	((*inputBoard)[7][0]).setPiece('R');
-	((*inputBoard)[7][0]).setColor('w');
+	((*inputBoard)[7][0]).setColor('b');
 
 	((*inputBoard)[7][1]).setPiece('N');
 	((*inputBoard)[7][1]).setColor('b');
@@ -93,7 +93,7 @@ int printBoard(vector<vector<square>> inputBoard)
 	printf("\n");
 	return 0;
 }
-bool isValidMove(int startCo[], int endCo[], vector<vector<square>> inputBoard, bool whitesTurn)
+bool isValidMove(int startCo[], int endCo[], vector<vector<square>> inputBoard, bool whitesTurn, vector<vector<int>> moveSet)
 {
 	
 	square *startingSquare = &(inputBoard[startCo[0]][startCo[1]]);
@@ -109,17 +109,191 @@ bool isValidMove(int startCo[], int endCo[], vector<vector<square>> inputBoard, 
 		return false;
 	}
 
-	//check for obstructions
+	//check if move is in moveSet
+
+
 
 	return true;
 }
 
-int generateMoveSet(int startingCo[2], char pieceChar, vector<vector<int>> *moveSet)
+int generateMoveSet(int startingCo[2], bool whitesTurn, vector<vector<square>> inputBoard, vector<vector<int>> *moveSet)
 {
+	square* startingSquare = &(inputBoard[startingCo[0]][startingCo[1]]);
+	if (startingSquare->getPiece() == EMPTY)  //check if piece exists in square
+	{
+		printf("no piece on that square.\n");
+		return 0;
+	}
+	//check if square contains right color piece.
+	else if ((whitesTurn && startingSquare->getColor() == 'b') || (!whitesTurn && startingSquare->getColor() == 'w'))
+	{
+		printf("You don't have a piece on that square.\n");
+		return 0;
+	}
 
+	vector<int> move(2);
+	char piece = startingSquare->getPiece();
+	switch (piece)
+	{
+	case 'N':
+		
+		move.at(0) = startingCo[0] + 2; move.at(1) = startingCo[1] + 1;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] + 1; move.at(1) = startingCo[1] + 2;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] - 2; move.at(1) = startingCo[1] - 1;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] - 1; move.at(1) = startingCo[1] - 2;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] + 1; move.at(1) = startingCo[1] - 2;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] + 2; move.at(1) = startingCo[1] - 1;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] - 2; move.at(1) = startingCo[1] + 1;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		move.at(0) = startingCo[0] - 1; move.at(1) = startingCo[1] + 2;
+		if (move.at(0) >= 0 && move.at(0) <= 7 && move.at(1) >= 0 && move.at(1) <= 7)
+		{
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+		}
+		break;
+	case 'R':
+		for (int i = startingCo[0] + 1; i < 8; ++i)
+		{
+			move.at(0) = i; move.at(1) = startingCo[1];
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+			else
+			{
+				//hit own piece, end loop
+				break;
+			}
+			if (inputBoard.at(move.at(0)).at(move.at(1)).getColor() != EMPTY)
+			{
+				//hit other piece, add move and end loop
+				break;
+			}
+		}
+		for (int i = startingCo[0] - 1; i >= 0; --i)
+		{
+			move.at(0) = i; move.at(1) = startingCo[1];
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+			else
+			{
+				//hit own piece, end loop
+				break;
+			}
+			if (inputBoard.at(move.at(0)).at(move.at(1)).getColor() != EMPTY)
+			{
+				//hit other piece, add move and end loop
+				break;
+			}
+		}
+		for (int i = startingCo[1] + 1; i < 8; ++i)
+		{
+			move.at(0) = startingCo[0]; move.at(1) = i;
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+			else
+			{
+				//hit own piece, end loop
+				break;
+			}
+			if (inputBoard.at(move.at(0)).at(move.at(1)).getColor() != EMPTY)
+			{
+				//hit other piece, add move and end loop
+				break;
+			}
+		}
+		for (int i = startingCo[1] - 1; i >= 0; --i)
+		{
+			move.at(0) = startingCo[0]; move.at(1) = i;
+			if (!((inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'w' && whitesTurn) || (inputBoard.at(move.at(0)).at(move.at(1)).getColor() == 'b' && !whitesTurn)))
+			{
+				moveSet->push_back(move);
+			}
+			else
+			{
+				//hit own piece, end loop
+				break;
+			}
+			if (inputBoard.at(move.at(0)).at(move.at(1)).getColor() != EMPTY)
+			{
+				//hit other piece, add move and end loop
+				break;
+			}
+		}
+		break;
+	case 'P':
+		break;
+	case 'B':
+		break;
+	case 'Q':
+		break;
+	case 'K':
 
+		break;
+	default:
+		printf("error, no piece there.");
+	}
+	
+	// generate original move set
 
-	return 0; //number of possible moves
+	// remove elements based on obstructions
+
+	return moveSet->size(); //number of possible moves -- unsigned int
 }
 
 int main()
@@ -170,16 +344,27 @@ int main()
 		square* startSquare = &(board[startCo[0]][startCo[1]]);
 
 		vector<vector<int>> moveSet;
-		generateMoveSet(startCo, startSquare->getPiece(), &moveSet);
+		if (generateMoveSet(startCo, whitesTurn, board, &moveSet) == 0)
+		{
+			printf("not a valid move\n");
+			//continue;
+		}
+		
+		//printf("%i, %i\n", moveSet.at(0).at(0), moveSet.at(0).at(1));
 
+		//DEBUG: print move set
+		for (unsigned int i = 0; i < moveSet.size(); ++i)
+		{
+			printf("%i, %i\n", moveSet.at(i).at(0), moveSet.at(i).at(1));
+		}
 
-		if (!isValidMove(startCo, endCo, board, whitesTurn))
+		if (!isValidMove(startCo, endCo, board, whitesTurn, moveSet))
 		{
 			printf("That is not a valid move.\n");
 			continue;
 		}
 
-		printf("%i    %i\n", startCo[0], startCo[1]);
+		//printf("%i    %i\n", startCo[0], startCo[1]);
 		//empty starting square and fill new square
 		
 		board[endCo[0]][endCo[1]].setColor(startSquare->getColor());
